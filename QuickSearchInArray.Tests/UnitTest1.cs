bbb62622dc29c545665d7ece5abd0c6d2d573598
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2021-10-07 22:18:43 +0400

    QuickSearchInArray - complete

---
+++ QuickSearchInArray.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using Xunit;
+
+namespace QuickSearchInArray.Tests
+{
+    public class UnitTest1
+    {
+        private const string Path = @"D:\Repos\Kudiyarov.YandexAlgorithms\QuickSearchInArray.Tests\TestCases\";
+        
+        [Theory]
+        [InlineData("1-input", "1-output")]
+        [InlineData("2-input", "2-output")]
+        public void Test1(string input, string output)
+        {
+            input = Path + input;
+            output = Path + output;
+            var lines = File.ReadAllLines(input);
+            var expected = File.ReadAllText(output);
+            var actual = Program.GetResult(lines);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ QuickSearchInArray/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace QuickSearchInArray
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            var result = GetResult(input);
+            File.WriteAllText("output.txt", result);
+        }
+
+        public static string GetResult(string[] lines)
+        {
+            var array = lines[1]
+                .Trim()
+                .Split()
+                .Select(long.Parse)
+                .OrderBy(number => number)
+                .ToList();
+
+            var requestsCount = int.Parse(lines[2].Trim());
+            var responses = new List<int>(requestsCount);
+
+            for (var i = 0; i < requestsCount; i++)
+            {
+                var request = lines[3 + i]
+                    .Trim()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToList();
+
+                var a = request[0];
+                var b = request[1];
+                
+                var isOutOfRange =
+                    a < array[0] && b < array[0] ||
+                    a > array[array.Count - 1] && b > array[array.Count - 1];
+
+                if (isOutOfRange)
+                {
+                    responses.Add(0);
+                    continue;
+                }
+                
+                var min = array.LeftBinarySearch(a);
+                var max = array.RightBinarySearch(b);
+                var response = max - min + 1;
+                responses.Add(response);
+            }
+
+            var result = string.Join(" ", responses);
+            return result;
+        }
+
+        private static int LeftBinarySearch(this IReadOnlyList<long> source, long param)
+        {
+            var l = 0;
+            var r = source.Count - 1;
+            
+            while (l < r)
+            {
+                var m = (l + r) / 2;
+                var value = source[m];
+
+                if (value >= param)
+                {
+                    r = m;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+            }
+            
+            return l;
+        }
+        
+        private static int RightBinarySearch(this IReadOnlyList<long> source, long param)
+        {
+            var l = 0;
+            var r = source.Count - 1;
+            
+            while (l < r)
+            {
+                var m = (l + r + 1) / 2;
+                var value = source[m];
+
+                if (value <= param)
+                {
+                    l = m;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+            }
+            
+            return l;
+        }
+    }
+}
