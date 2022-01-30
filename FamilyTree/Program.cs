1fad6a902bff3fb1b66928fe5bb68f219d2a2103
Aleksandr Kudiyarov <alexxkudi@gmail.com>
2022-01-30 13:23:37 +0400

    FamiliTree completed

--- FamilyTree/Program.cs
+++ FamilyTree/Program.cs
@@ -3,123 +3,120 @@ using System.IO;
 
 namespace FamilyTree
 {
-     public static class Program
-     {
-          private static void Main()
-          {
-               var input = File.ReadAllLines("input.txt");
-               var result = GetResult(input);
-               File.WriteAllText("output.txt", result);
-          }
+    public static class Program
+    {
+        private static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            var result = GetResult(input);
+            File.WriteAllText("output.txt", result);
+        }
 
-          public static string GetResult(string[] input)
-          {
-               var count = int.Parse(input[0]);
-               var tree = new FamilyTree(count - 1);
-               FillTree(tree, input, count);
+        public static string GetResult(string[] input)
+        {
+            var count = int.Parse(input[0]);
+            var tree = new FamilyTree(count - 1);
+            FillTree(tree, input, count);
                
-               var relations = GetRelations(tree, input, count);
-               var output = string.Join(" ", relations);
-               return output;
-          }
+            var relations = GetRelations(tree, input, count);
+            var output = string.Join(" ", relations);
+            return output;
+        }
 
-          private static IEnumerable<string> GetRelations(FamilyTree tree, IReadOnlyList<string> input, int count)
-          {
-               for (var i = count; i < input.Count; i++)
-               {
-                    var splitted = input[i].Split();
-                    var first = splitted[0];
-                    var second = splitted[1];
-                    var relation = tree.GetRelation(first, second);
-                    yield return relation.ToString();
-               }
-          }
+        private static IEnumerable<string> GetRelations(FamilyTree tree, IReadOnlyList<string> input, int count)
+        {
+            for (var i = count; i < input.Count; i++)
+            {
+                var splitted = input[i].Split();
+                var first = splitted[0];
+                var second = splitted[1];
+                var relation = tree.GetRelation(first, second);
+                yield return relation;
+            }
+        }
 
-          private static void FillTree(FamilyTree tree, IReadOnlyList<string> input, int count)
-          {
-               for (var i = 1; i < count; i++)
-               {
-                    var splitted = input[i].Split();
-                    var children = splitted[0];
-                    var parent = splitted[1];
-                    tree.Add(children, parent);
-               }
-          }
-     }
+        private static void FillTree(FamilyTree tree, IReadOnlyList<string> input, int count)
+        {
+            for (var i = 1; i < count; i++)
+            {
+                var splitted = input[i].Split();
+                var children = splitted[0];
+                var parent = splitted[1];
+                tree.Add(children, parent);
+            }
+        }
+    }
 
-     public class FamilyTree
-     {
-          private readonly Dictionary<string, Node<string>> _dictionary;
+    public class FamilyTree
+    {
+        private readonly Dictionary<string, Node<string>> _dictionary;
 
-          public FamilyTree(int capacity)
-          {
-               _dictionary = new Dictionary<string, Node<string>>(capacity);
-          }
+        public FamilyTree(int capacity)
+        {
+            _dictionary = new Dictionary<string, Node<string>>(capacity);
+        }
 
-          public void Add(string childrenName, string parentName)
-          {
-               var parentNode = GetParentNode(parentName);
+        public void Add(string childrenName, string parentName)
+        {
+            var parentNode = GetNode(parentName);
+            var childrenNode = GetNode(childrenName);
+            childrenNode.Parent = parentNode;
+        }
 
-               var childrenNode = new Node<string>
-               {
-                    Parent = parentNode,
-                    Value = childrenName
-               };
+        public string GetRelation(string first, string second)
+        {
+            var firstNode = _dictionary[first];
 
-               _dictionary[childrenName] = childrenNode;
-          }
+            if (TryGetParent(second, firstNode))
+            {
+                return "2";
+            }
 
-          public int GetRelation(string first, string second)
-          {
-               var firstNode = _dictionary[first];
+            var secondNode = _dictionary[second];
 
-               if (TryGetParent(second, firstNode))
-               {
-                    return 2;
-               }
+            if (TryGetParent(first, secondNode))
+            {
+                return "1";
+            }
 
-               var secondNode = _dictionary[second];
+            return "0";
+        }
 
-               if (TryGetParent(first, secondNode))
-               {
-                    return 1;
-               }
-
-               return 0;
-          }
-
-          private static bool TryGetParent(string name, Node<string> node)
-          {
-               if (node == null)
-               {
+        private static bool TryGetParent(string name, Node<string> node)
+        {
+            while (true)
+            {
+                if (node == null)
+                {
                     return false;
-               }
+                }
 
-               if (node.Value == name)
-               {
+                if (node.Value == name)
+                {
                     return true;
-               }
+                }
 
-               return TryGetParent(name, node.Parent);
-          }
+                node = node.Parent;
+            }
+        }
 
-          private Node<string> GetParentNode(string parentName)
-          {
-               Node<string> parentNode;
+        private Node<string> GetNode(string name)
+        {
+            Node<string> parentNode;
                
-               if (!_dictionary.TryGetValue(parentName, out parentNode))
-               {
-                    parentNode = new Node<string> { Value = parentName };
-                    _dictionary[parentName] = parentNode;
-               }
+            if (!_dictionary.TryGetValue(name, out parentNode))
+            {
+                parentNode = new Node<string> { Value = name };
+                _dictionary[name] = parentNode;
+            }
 
-               return parentNode;
-          }
-     }
+            return parentNode;
+        }
+    }
 
-     public class Node<T>
-     {
-          public Node<T> Parent { get; set; }
-          public T Value { get; set; }
-     }
+    public class Node<T>
+    {
+        public Node<T> Parent { get; set; }
+        public T Value { get; set; }
+    }
 }
